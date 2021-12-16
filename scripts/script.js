d309f1430db26f51f0bbb8f64bb73cefe6557cc3
Richnei <rich.nei@hotmail.com>
2021-12-16 19:06:21 -0300

    css_html

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 // Carne - 400 gr por pessoa  + de 6 horas - 650
 // Cerveja - 1200 ml por pessoa + 6 horas - 2000 ml
 // Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500ml
-// Doces e salgados - 10 por pessoa + 6 horas 15
 //crianças valem por 0,5
